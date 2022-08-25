66544f068df781058dcfa1e74ed2e9048d99b995
ericrothmuller <eric@skymarketingco.com>
2022-08-24 23:06:18 -0700

    Added footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
+import Footer from './components/Footer';
 import './Style.css';
 
 // Pass users array to the List component as a prop
@@ -7,6 +8,7 @@ export default function App() {
   return (
     <div>
       <Header />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../Style.css';
+
+export default function Footer() {
+
+  return (
+    <footer>
+      <p>
+        Copyright Eric Rothmuller 2022
+      </p>
+    </footer>
+  );
+}
