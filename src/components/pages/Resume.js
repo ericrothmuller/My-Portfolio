5161170fa03fa17ab8624b92dd1895256a4760ef
ericrothmuller <eric@skymarketingco.com>
2022-09-19 23:36:25 -0700

    Added Download PDF button for my resume which links to the resume pdf and added button CSS stylings

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../../Style.css';
-import ResumeImage from '../../images/Eric-Rothmuller-Resume.jpg'
+import ResumeImage from '../../images/Eric-Rothmuller-Resume.jpg';
+import ResumePdf from '../../images/Eric-Rothmuller-Resume.pdf';
 
 export default function Resume() {
 
@@ -9,6 +10,8 @@ export default function Resume() {
       <h1>Eric Rothmuller's Resume</h1>
       <div className="resumeDiv">
         <img src={ResumeImage} className="resume" alt="Resume" title="Click to download PDF" />
+        
+        <a name="Pdfdownload" href={ResumePdf} target="_blank"><button className="PdfButton">Download PDF</button></a>
       </div>
     </div>
   );
