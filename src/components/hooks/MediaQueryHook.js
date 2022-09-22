2c2d6495482286d0b5086174f566ed91276c5a15
ericrothmuller <eric@skymarketingco.com>
2022-09-22 09:11:22 -0700

    Created a media query hook so that it can be used throughout the website for responsive design within in-line stylings

---
+++ src/components/hooks/MediaQueryHook.js
@@ -0,0 +1,13 @@
+import { useEffect, useState } from "react";
+
+export const mediaQuery = (query) => {
+    const mediaQueryMatch = window.matchMedia(query);
+    const [matches, setMatches] = useState(mediaQueryMatch.matches);
+  
+    useEffect(() => {
+      const handler = e => setMatches(e.matches);
+      mediaQueryMatch.addEventListener(handler);
+      return () => mediaQueryMatch.removeEventListener(handler);
+    });
+    return matches;
+  };
