6909b699a44b26b91cb2d88eb699f93613e4f4fb
ericrothmuller <eric@skymarketingco.com>
2022-09-22 01:23:03 -0700

    Added 4 portoflio item screen shots with alt and title attributes

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -5,7 +5,7 @@ const portfolioCard = {
   width: "45%",
   padding: "25px",
   borderRadius: "35px",
-  cursor: "pointer",
+  cursor: "pointer"
 }
 
 export default function Portfolio() {
@@ -18,9 +18,9 @@ export default function Portfolio() {
       </p>
       <div className="cardsDiv">
         <img src={require("../../images/Cocktail-Concoctor-Screen-Shot.jpg")} alt="Cocktail Concocter Portfolio" title="Cocktail Concocter" style={portfolioCard} />
-        <img src={require("../../images/Cocktail-Concoctor-Screen-Shot.jpg")} alt="Cocktail Concocter Portfolio" title="Cocktail Concocter" style={portfolioCard} />
-        <img src={require("../../images/Cocktail-Concoctor-Screen-Shot.jpg")} alt="Cocktail Concocter Portfolio" title="Cocktail Concocter" style={portfolioCard} />
-        <img src={require("../../images/Cocktail-Concoctor-Screen-Shot.jpg")} alt="Cocktail Concocter Portfolio" title="Cocktail Concocter" style={portfolioCard} />
+        <img src={require("../../images/Jedi-Clicker-Screen-Shot.jpg")} alt="Jedi Clicker Portfolio" title="Jedi Clicker" style={portfolioCard} />
+        <img src={require("../../images/Elite-MD-Screenshot.jpg")} alt="Elite MD Portfolio" title="Elite MD" style={portfolioCard} />
+        <img src={require("../../images/Murder-Ridge-Screenshot.jpg")} alt="Murder Ridge Portfolio" title="Murder Ridge" style={portfolioCard} />
       </div>
     </div>
   );
