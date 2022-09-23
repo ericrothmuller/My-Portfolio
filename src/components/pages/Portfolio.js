75123dada4b84b9eb79b279accc44ab7f5f4b304
ericrothmuller <eric@skymarketingco.com>
2022-09-22 21:51:31 -0700

    Added handleClick function to portfolio image

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -5,6 +5,10 @@ const centerP = {
   textAlign: "center",
 }
 
+const handleClick = event => {
+  console.log("image clicked")
+};
+
 export default function Portfolio() {
 
 
@@ -16,7 +20,7 @@ export default function Portfolio() {
         Here is a list of some of the projects I've worked on. You can click on each image for more information on each project.
       </p>
       <div className="cardsDiv">
-        <img className ="portfolioImage" src={require("../../images/Cocktail-Concoctor-Screen-Shot.jpg")} alt="Cocktail Concocter Portfolio" title="Cocktail Concocter" />
+        <img className ="portfolioImage" src={require("../../images/Cocktail-Concoctor-Screen-Shot.jpg")} alt="Cocktail Concocter Portfolio" title="Cocktail Concocter" onClick={handleClick} />
         <img className ="portfolioImage" src={require("../../images/Jedi-Clicker-Screen-Shot.jpg")} alt="Jedi Clicker Portfolio" title="Jedi Clicker" />
         <img className ="portfolioImage" src={require("../../images/Elite-MD-Screenshot.jpg")} alt="Elite MD Portfolio" title="Elite MD" />
         <img className ="portfolioImage" src={require("../../images/Murder-Ridge-Screenshot.jpg")} alt="Murder Ridge Portfolio" title="Murder Ridge" />
