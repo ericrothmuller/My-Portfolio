2cc93331d21cfe7e37eba9ee7c79fc71b33bb639
ericrothmuller <eric@skymarketingco.com>
2022-09-23 22:27:16 -0700

    Added CSS stylings to the portfolio descriptions.

--- src/components/pages/portfolioItems/CocktailConcoctor.js
+++ src/components/pages/portfolioItems/CocktailConcoctor.js
@@ -5,10 +5,10 @@ export default function CocktailConcoctor() {
 
     return (
         <div className="portfolioDetails">
-<strong>Description:</strong> A web application that finds cocktail recipes based on input ingredients.<br />
-<strong>Built for:</strong> U.C. Berkeley Coding Bootcamp<br />
-<strong>Built with:</strong> HTML, CSS, JavaScript, Tailwind, The CocktailDB API, YouTube API, Google Fonts API, Awesomplete API, and GitHub.<br />
-<strong>Deployed link:</strong> <a href="https://ericrothmuller.github.io/Cocktail-Concocter/">ericrothmuller.github.io/Cocktail-Concocter/</a><br />
+<p><strong>Description:</strong> A web application that finds cocktail recipes based on input ingredients.</p>
+<p><strong>Built for:</strong> U.C. Berkeley Coding Bootcamp</p>
+<p><strong>Built with:</strong> HTML, CSS, JavaScript, Tailwind, The CocktailDB API, YouTube API, Google Fonts API, Awesomplete API, and GitHub.</p>
+<p><strong>Deployed link:</strong> <a href="https://ericrothmuller.github.io/Cocktail-Concocter/">ericrothmuller.github.io/Cocktail-Concocter/</a></p>
         </div>
     )
 }
--- src/components/pages/portfolioItems/JediClicker.js
+++ src/components/pages/portfolioItems/JediClicker.js
@@ -5,10 +5,10 @@ export default function JediClicker() {
 
     return (
         <div className="portfolioDetails">
-<strong>Description:</strong> A Star Wars based clicker game.<br />
-<strong>Built for:</strong> U.C. Berkeley Coding Bootcamp<br />
-<strong>Built with:</strong> HTML, CSS, JavaScript, Node, Express, MySQL2, Bcrypt, Sequelize, Connect Session Sequelize, Dotenv, Express Handlebars, Handlebars, Paint.net, and GitHub.<br />
-<strong>Deployed link:</strong> <a href="https://jediclicker.herokuapp.com/login">jediclicker.herokuapp.com/login</a><br />
+<p><strong>Description:</strong> A Star Wars based clicker game.</p>
+<p><strong>Built for:</strong> U.C. Berkeley Coding Bootcamp</p>
+<p><strong>Built with:</strong> HTML, CSS, JavaScript, Node, Express, MySQL2, Bcrypt, Sequelize, Connect Session Sequelize, Dotenv, Express Handlebars, Handlebars, Paint.net, and GitHub.</p>
+<p><strong>Deployed link:</strong> <a href="https://jediclicker.herokuapp.com/login">jediclicker.herokuapp.com/login</a></p>
         </div>
     )
 }
