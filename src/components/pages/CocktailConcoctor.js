934739996409460758392870e5c3841960f9fe95
ericrothmuller <eric@skymarketingco.com>
2022-09-23 21:23:35 -0700

    Added cocktail concoctor description for drop-down along with CSS and div changes to style and make responsive.

--- src/components/pages/CocktailConcoctor.js
+++ src/components/pages/CocktailConcoctor.js
@@ -1,9 +1,13 @@
 import React from "react";
+import '../../Style.css';
 
 export default function CocktailConcoctor() {
     return (
-        <div>
-        "test"
+        <div className="portfolioDetails">
+<strong>Description:</strong> A web application that finds cocktail recipes based on input ingredients.<br />
+<strong>Built for:</strong> U.C. Berkeley Coding Bootcamp<br />
+<strong>Built with:</strong> HTML, CSS, JavaScript, Tailwind, The CocktailDB API, YouTube API, Google Fonts API, and Awesomplete API.<br />
+<strong>Deployed link:</strong> <a href="https://ericrothmuller.github.io/Cocktail-Concocter/">ericrothmuller.github.io/Cocktail-Concocter/</a>
         </div>
     )
 }
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -17,11 +17,19 @@ const [imageOneIsOpen, toggleImageOne] = useState(false);
         Here is a list of some of the projects I've worked on. You can click on each image for more information on each project.
       </p>
       <div className="cardsDiv">
-        <img className="portfolioImage" src={require("../../images/Cocktail-Concoctor-Screen-Shot.jpg")} alt="Cocktail Concocter Portfolio" title="Cocktail Concocter" onClick={() => toggleImageOne(true)} />
-        {imageOneIsOpen ? <CocktailConcoctor /> : null}
-        <img className="portfolioImage" src={require("../../images/Jedi-Clicker-Screen-Shot.jpg")} alt="Jedi Clicker Portfolio" title="Jedi Clicker" />
-        <img className="portfolioImage" src={require("../../images/Elite-MD-Screenshot.jpg")} alt="Elite MD Portfolio" title="Elite MD" />
-        <img className="portfolioImage" src={require("../../images/Murder-Ridge-Screenshot.jpg")} alt="Murder Ridge Portfolio" title="Murder Ridge" />
+        <div className="portfolioDiv">
+          <img className="portfolioImage" src={require("../../images/Cocktail-Concoctor-Screen-Shot.jpg")} alt="Cocktail Concocter Portfolio" title="Cocktail Concocter" onClick={() => toggleImageOne(true)} />
+          {imageOneIsOpen ? <CocktailConcoctor /> : null}
+        </div>
+        <div className="portfolioDiv">
+          <img className="portfolioImage" src={require("../../images/Jedi-Clicker-Screen-Shot.jpg")} alt="Jedi Clicker Portfolio" title="Jedi Clicker" />
+        </div>
+        <div className="portfolioDiv">
+          <img className="portfolioImage" src={require("../../images/Elite-MD-Screenshot.jpg")} alt="Elite MD Portfolio" title="Elite MD" />
+        </div>
+        <div className="portfolioDiv">
+          <img className="portfolioImage" src={require("../../images/Murder-Ridge-Screenshot.jpg")} alt="Murder Ridge Portfolio" title="Murder Ridge" />
+        </div>
       </div>
     </div>
   );
