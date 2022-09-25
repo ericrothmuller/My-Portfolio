617fa01998c30a0a11cb807bbfc1427390523e95
ericrothmuller <eric@skymarketingco.com>
2022-09-24 23:46:19 -0700

    Added animate.css package to the web app with an animation on the contact page.

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import '../../Style.css';
+import "animate.css";
+
 
 export default function Contact() {
 
   return (
-    <div className='contactMe'>
+    <div className="contactMe animate__animated animate__bounce">
       <h1>Get In Contact</h1>
       <p>You can reach me by phone or email at:</p>
       <p><strong>Phone: </strong><a href="tel:17078432047">707-843-2047</a></p>
