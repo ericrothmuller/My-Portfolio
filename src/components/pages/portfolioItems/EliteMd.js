d304ee9434a6ac058fc15350a3398e780f491cbd
ericrothmuller <eric@skymarketingco.com>
2022-09-26 11:33:28 -0700

    Updated CSS animations to a more clean look

--- src/components/pages/portfolioItems/EliteMd.js
+++ src/components/pages/portfolioItems/EliteMd.js
@@ -4,11 +4,11 @@ import '../../../Style.css';
 export default function EliteMd() {
 
     return (
-        <div className="portfolioDetails animate__animated animate__fadeIn">
-<p className="animate__animated animate__slideInLeft"><strong>Description:</strong> A business website for a plastic surgery and dermatology company.</p>
-<p className="animate__animated animate__slideInRight"><strong>Built for:</strong> Elite MD</p>
-<p className="animate__animated animate__slideInLeft"><strong>Built with:</strong> HTML, CSS, Wordpress, WooCommerce, and paint.net.</p>
-<p className="animate__animated animate__slideInRight"><strong>Deployed link:</strong> <a href="https://elitemdspa.com">elitemdspa.com</a></p>
+        <div className="portfolioDetails animate__animated animate__fadeIn animate__faster">
+<p><strong>Description:</strong> A business website for a plastic surgery and dermatology company.</p>
+<p><strong>Built for:</strong> Elite MD</p>
+<p><strong>Built with:</strong> HTML, CSS, Wordpress, WooCommerce, and paint.net.</p>
+<p><strong>Deployed link:</strong> <a href="https://elitemdspa.com">elitemdspa.com</a></p>
         </div>
     )
 }
--- src/components/pages/portfolioItems/JediClicker.js
+++ src/components/pages/portfolioItems/JediClicker.js
@@ -4,11 +4,11 @@ import '../../../Style.css';
 export default function JediClicker() {
 
     return (
-        <div className="portfolioDetails animate__animated animate__fadeIn">
-<p className="animate__animated animate__slideInLeft"><strong>Description:</strong> A Star Wars based clicker game.</p>
-<p className="animate__animated animate__slideInRight"><strong>Built for:</strong> U.C. Berkeley Coding Bootcamp</p>
-<p className="animate__animated animate__slideInLeft"><strong>Built with:</strong> HTML, CSS, JavaScript, Node, Express, MySQL2, Bcrypt, Sequelize, Connect Session Sequelize, Dotenv, Express Handlebars, Handlebars, Paint.net, and GitHub.</p>
-<p className="animate__animated animate__slideInRight"><strong>Deployed link:</strong> <a href="https://jediclicker.herokuapp.com/login">jediclicker.herokuapp.com/login</a></p>
+        <div className="portfolioDetails animate__animated animate__fadeIn animate__faster">
+<p><strong>Description:</strong> A Star Wars based clicker game.</p>
+<p><strong>Built for:</strong> U.C. Berkeley Coding Bootcamp</p>
+<p><strong>Built with:</strong> HTML, CSS, JavaScript, Node, Express, MySQL2, Bcrypt, Sequelize, Connect Session Sequelize, Dotenv, Express Handlebars, Handlebars, Paint.net, and GitHub.</p>
+<p><strong>Deployed link:</strong> <a href="https://jediclicker.herokuapp.com/login">jediclicker.herokuapp.com/login</a></p>
         </div>
     )
 }
