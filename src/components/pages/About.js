8e423edd8b4169b41bce4a5934e3ba54b6d77853
ericrothmuller <eric@skymarketingco.com>
2022-09-28 08:19:30 -0700

    Changed dev and marketing skills buttons divs and classes to make more sense

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -6,13 +6,13 @@ import '../../Style.css';
 export default function About() {
 
   const viewDevSkills = () => {
-    document.getElementById("leftDiv").classList.remove('leftDivHid');
-    document.getElementById("leftDiv").classList.add('leftDiv');
+    document.getElementById("devDiv").classList.remove('hiddenDiv');
+    document.getElementById("devDiv").classList.add('visibleDiv');
   };
 
   const viewMarkSkills = () => {
-    document.getElementById("rightDiv").classList.remove('rightDivHid');
-    document.getElementById("rightDiv").classList.add('rightDiv');
+    document.getElementById("marketingDiv").classList.remove('hiddenDiv');
+    document.getElementById("marketingDiv").classList.add('visibleDiv');
   };
 
   return (
@@ -26,54 +26,56 @@ export default function About() {
       <img className="aboutImage animate__animated animate__slideInRight" src={require("../../images/Eric.jpg")} alt="Eric Rothmuller Headshot" title="Eric Rothmuller" />
     </div>
 
+    {/* SKILLS BUTTONS */}
+
     <div className="skillsButtons">
-      <div className="skillsButtonCenter">
-    <button className="developerSkillsButton" onClick={viewDevSkills}>Developer Skills</button>
-    <button className="marketingSkillsButton" onClick={viewMarkSkills}>Marketing Skills</button>
-      </div>
-
       <div className="columnDiv">
-    <div id="leftDiv" className="leftDivHid">
-      <h2>Developer Skills:</h2>
-    <ul>
-        <li className='normalUl'>HTML</li>
-        <li className='normalUl'>CSS</li>
-        <li className='normalUl'>JavaScript</li>
-        <li className='normalUl'>React</li>
-        <li className='normalUl'>Responsive Design</li>
-        <li className='normalUl'>SQL</li>
-        <li className='normalUl'>Node.js</li>
-        <li className='normalUl'>Express.js</li>
-        <li className='normalUl'>MongoDB</li>
-        <li className='normalUl'>Mongoose</li>
-        <li className='normalUl'>REST APIs</li>
-        <li className='normalUl'>Third-party APIs</li>
-        <li className='normalUl'>Mongoose</li>
-        <li className='normalUl'>Git and GitHub</li>
-        <li className='normalUl'>Dev Tools</li>
-      </ul>
-    </div>
-
-    <div id="rightDiv" className="rightDivHid">
-    <h2>Marketing Skills:</h2>
-    <ul>
-        <li className='normalUl'>Search Engine Optimization</li>
-        <li className='normalUl'>Google Analytics</li>
-        <li className='normalUl'>Google Search Console</li>
-        <li className='normalUl'>Google My Business</li>
-        <li className='normalUl'>Google Ads</li>
-        <li className='normalUl'>WordPress</li>
-        <li className='normalUl'>WooCommerce</li>
-        <li className='normalUl'>eCommerce Product Management</li>
-        <li className='normalUl'>Content Writing</li>
-        <li className='normalUl'>Graphic Design</li>
-        <li className='normalUl'>Email Marketing</li>
-        <li className='normalUl'>Social Media Marketing</li>
-        <li className='normalUl'>Lighthouse</li>
-      </ul>
-    </div>
-    </div>
-    </div>
+      <button className="developerSkillsButton" onClick={viewDevSkills}>Developer Skills</button>
+      <button className="marketingSkillsButton" onClick={viewMarkSkills}>Marketing Skills</button>
+        <div id="leftDiv" className="leftDiv">
+          <div id="devDiv" className="hiddenDiv">
+            <h2>Developer Skills:</h2>
+            <ul>
+                <li className='normalUl'>HTML</li>
+                <li className='normalUl'>CSS</li>
+                <li className='normalUl'>JavaScript</li>
+                <li className='normalUl'>React</li>
+                <li className='normalUl'>Responsive Design</li>
+                <li className='normalUl'>SQL</li>
+                <li className='normalUl'>Node.js</li>
+                <li className='normalUl'>Express.js</li>
+                <li className='normalUl'>MongoDB</li>
+                <li className='normalUl'>Mongoose</li>
+                <li className='normalUl'>REST APIs</li>
+                <li className='normalUl'>Third-party APIs</li>
+                <li className='normalUl'>Mongoose</li>
+                <li className='normalUl'>Git and GitHub</li>
+                <li className='normalUl'>Dev Tools</li>
+              </ul>
+            </div>
+        </div>
+      <div id="rightDiv" className="rightDiv">
+        <div id="marketingDiv" className="hiddenDiv">
+          <h2>Marketing Skills:</h2>
+          <ul>
+              <li className='normalUl'>Search Engine Optimization</li>
+              <li className='normalUl'>Google Analytics</li>
+              <li className='normalUl'>Google Search Console</li>
+              <li className='normalUl'>Google My Business</li>
+              <li className='normalUl'>Google Ads</li>
+              <li className='normalUl'>WordPress</li>
+              <li className='normalUl'>WooCommerce</li>
+              <li className='normalUl'>eCommerce Product Management</li>
+              <li className='normalUl'>Content Writing</li>
+              <li className='normalUl'>Graphic Design</li>
+              <li className='normalUl'>Email Marketing</li>
+              <li className='normalUl'>Social Media Marketing</li>
+              <li className='normalUl'>Lighthouse</li>
+            </ul>
+          </div>
+      </div>
+      </div>
+      </div>
 
     </div>
     
