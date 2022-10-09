acaddc59349e689629aba059f94ec61ba18caa1c
ericrothmuller <eric@skymarketingco.com>
2022-10-09 01:33:43 -0700

    Updated contact page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -2,11 +2,31 @@ import React from "react";
 import "../../Style.css";
 import "animate.css";
 
+const blackColorTop = {
+  color: "black",
+  fontWeight: "700",
+  paddingBottom: "15px",
+};
+
+const blackColorBottom = {
+  color: "black",
+  fontWeight: "700",
+  paddingTop: "15px",
+};
+
 export default function Contact() {
   return (
     <div className="contactMe animate__animated animate__fadeIn">
+      <div className="aboutImageDiv">
+        <img
+          className="contactImage"
+          src={require("../../images/Eric.jpg")}
+          alt="Eric Rothmuller Headshot"
+          title="Eric Rothmuller"
+        />
+      </div>
       <h1>Get In Contact</h1>
-      <p>You can reach me by phone or email at:</p>
+      <p style={blackColorTop}>You can reach me by phone or email at:</p>
       <p>
         <strong>Phone: </strong>
         <a href="tel:17078432047">707-843-2047</a>
@@ -15,6 +35,7 @@ export default function Contact() {
         <strong>Email: </strong>
         <a href="mailto:ericrothmuller@gmail.com">ericrothmuller@gmail.com</a>
       </p>
+      <p style={blackColorBottom}>Thank you for your interest!</p>
     </div>
   );
 }
